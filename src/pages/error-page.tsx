c3eab62e51b3494ea765fd2027b211d3718bf0f6
Damian Kucharski <damiankuchar1@gmail.com>
2024-10-22 01:11:38 +0200

    Error page and NotFound page improvement

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="h-svh">
+      <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-[7rem] font-bold leading-tight">500</h1>
+        <span className="font-medium">Oops! Something went wrong :')</span>
+        <p className="text-center text-muted-foreground">
+          We apologize for the inconvenience. <br />
+          Please try again later.
+        </p>
+        <div className="mt-6 flex gap-4">
+          <Button variant="outline" onClick={() => navigate(-1)}>
+            Go Back
+          </Button>
+          <Button onClick={() => navigate("/")}>Back to Home</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
